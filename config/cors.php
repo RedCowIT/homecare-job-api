756e31d92dc8ce597b18c8500170c93136cdf9f9
Dominic Foulkes <dom@digi-dom.co.uk>
2022-02-17 12:47:06 +0000

    chore: explicit CORS origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost',
+        'https://homecare.digi-dom.co.uk:8100'
+    ],
 
     'allowed_origins_patterns' => [],
 
